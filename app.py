f9ce1454906bedd4ce90f8310107e3b567e8b7d5
Q. Price <priceqp@gmail.com>
2020-08-22 18:58:43 -0500

    implement Cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-"""Flask app for Cupcakes"""
+from flask import Flask, render_template, redirect, flash, request
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+# Configure and Initialize Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
