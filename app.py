be8c7f9382461aad44a732427362496993b82512
Q. Price <priceqp@gmail.com>
2020-08-22 20:35:50 -0500

    complete part two

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, jsonify
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
@@ -9,3 +9,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+  all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+  return jsonify(cupcakes=all_cupcakes)
+
+@app.route("/api/cupcakes/<int:id>")
+def get_cupcake(id):
+  cupcake = Cupcake.query.get_or_404(id)
+  return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+  new_cupcake = Cupcake(
+    flavor=request.json["flavor"], 
+    size=request.json["size"], 
+    rating=request.json["rating"], 
+    image=request.json.get("image", None))
+  db.session.add(new_cupcake)
+  db.session.commit()
+  response_json = jsonify(cupcake=new_cupcake.serialize())
+  return (response_json, 201)
